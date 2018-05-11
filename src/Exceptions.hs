aa520a998016b8521012ee99a43362ec6a8e90ec
HirotoShioi <hiroto.shioi@iohk.io>
2018-05-11 17:39:33 +0900

    Update comment

--- src/Exceptions.hs
+++ src/Exceptions.hs
@@ -8,11 +8,16 @@ import           Data.Typeable (Typeable)
 
 -- | Exception message describing each exceptions
 data KBError =
-      EmptyDirectory
-    | ParseError KBError FilePath -- ^ Parsing Error
-    | InvalidCategory Text     -- ^ Invalid category
-    | InvalidFormat               -- ^ Invalid format in the markdown
-    | InvalidLocale Text       -- ^ Invalid locale is being written down
+      EmptyDirectory              
+    -- ^ Empty Directory
+    | ParseError KBError FilePath
+    -- ^ Parsing Error
+    | InvalidCategory Text
+    -- ^ Invalid category
+    | InvalidFormat               
+    -- ^ Invalid format in the markdown
+    | InvalidLocale Text          
+    -- ^ Invalid locale is being written down
     deriving (Show, Typeable)
 
 instance Exception KBError
